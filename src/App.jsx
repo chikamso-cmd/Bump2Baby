987ccc2188d6fd72bffd198f3258c00c4c484159
cloneshoes <igweuche42@gmail.com>
2026-01-14 15:14:23 +0100

    Resolve merge conflict in App.js by combining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-// 1. IMPORT LOGIN COMPONENT HERE
 import Login from './components/Login'; 
 import Home from './components/home/Home';
 import MainRender from './dashboard/MainRender';
 import LandingPage from './landingpage/LandingPAge';
 import Profile from './profile/Profile';
+import CommunityFeed from './dashboard/components/communityFeed';
+import CreatePost from './dashboard/components/communityCreatePost';
+import HospitalFinder from './hospital/HospitalFinder';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Landing Page */}
+        {/* Landing & Auth */}
         <Route path="/" element={<LandingPage />} />
-        
-        {/* Login Page */}
         <Route path="/login" element={<Login />} />
         
         {/* Onboarding Flow */}
         <Route path="/home" element={<Home />} />
         
-        {/* Main Dashboard */}
+        {/* Main Dashboard - We keep /app as the primary route */}
         <Route path="/app" element={<MainRender />} />
+        <Route path="/dashboard" element={<Navigate to="/app" replace />} />
         
         {/* Profile */}
         <Route path="/profile" element={<Profile />} />
         
-        {/* Redirects */}
-        <Route path="/dashboard" element={<Navigate to="/app" replace />} />
+        {/* Community & Features from Main */}
+        <Route path="/community" element={<CommunityFeed />} />
+        <Route path="/community/create" element={<CreatePost />} />
+        <Route path="/hospital" element={<HospitalFinder />} />
 
         {/* 404 Redirect */}
         <Route path="*" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
